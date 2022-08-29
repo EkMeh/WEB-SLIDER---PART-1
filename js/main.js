831dd85e93bbb4a3f267f7e117c5abbd0670316d
Ekrem Mehmedov <96689375+EkMeh@users.noreply.github.com>
2022-08-29 20:06:32 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Slider code start
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+// Slider code end
